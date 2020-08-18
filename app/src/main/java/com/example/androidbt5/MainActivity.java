34da28c30ca64cde97017d06e8d4d88327ef1254
Hanabi <nguyenkientrung0412@gmail.com>
2020-08-18 07:25:01 +0700

    Done

---
+++ app/src/main/java/com/example/androidbt5/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.androidbt5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXTRA_RESULT = "extra.RESULT";
+    private static final int[] idArray = {R.id.tv_box_1, R.id.tv_box_2, R.id.tv_box_3, R.id.tv_box_4, R.id.tv_box_5, R.id.tv_box_6, R.id.tv_box_7, R.id.tv_box_8, R.id.tv_box_9};
+    private Button[] buttons = new Button[9];
+    private Button button;
+    private TextView tvNamePlayer;
+    private TextView tvTimes;
+    private int time = 5;
+    private int rs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+
+        for (int i = 0; i < idArray.length; i++) {
+            buttons[i] = findViewById(idArray[i]);
+            buttons[i].setOnClickListener(this);
+        }
+        String namePlayer = getIntent().getStringExtra(MenuActivity.EXTRA_PLAYER_NAME);
+        tvNamePlayer = findViewById(R.id.tv_player_name);
+        tvNamePlayer.setText("Tên người chơi: " + namePlayer);
+        tvTimes = findViewById(R.id.tv_times);
+        setTimes();
+    }
+
+    @Override
+    public void onClick(View view) {
+        button = findViewById(view.getId());
+        if (button.getTag() == null && time > 0) {
+            Random random = new Random();
+            int num = random.nextInt(11);
+            button.setBackgroundColor(Color.parseColor("#00B10E"));
+            button.setText(Integer.toString(num));
+            button.setTag("1");
+            rs += num;
+            time--;
+            setTimes();
+            if (time == 0) {
+                if (youWin()) {
+                    Toast.makeText(this, "Bạn thắng", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Bạn thua", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (time > 0) {
+            Toast.makeText(this, "Bạn không thể thoát khi chưa chơi xong", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = new Intent(this, MenuActivity.class);
+            intent.putExtra(EXTRA_RESULT, youWin());
+            setResult(Activity.RESULT_CANCELED, intent);
+            finish();
+        }
+
+    }
+
+    private void setTimes() {
+        tvTimes.setText("Times: " + time);
+    }
+
+    private boolean youWin() {
+        if (time == 0 && rs > 40) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/androidbt5/MenuActivity.java
@@ -0,0 +1,66 @@
+package com.example.androidbt5;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXTRA_PLAYER_NAME = "extra.PLAYER_NAME";
+    public static final int REQUEST_CODE_EXAMPLE = 1;
+
+    private EditText editPlayerName;
+    private Button btnStart;
+    private TextView tvTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        initViews();
+    }
+
+    private void initViews() {
+        editPlayerName = findViewById(R.id.edt_playername);
+        btnStart = findViewById(R.id.btn_start);
+        tvTitle = findViewById(R.id.tv_title);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String playerName = editPlayerName.getText().toString();
+        if (playerName.isEmpty()) {
+            Toast.makeText(this, "Không được để trống tên", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(EXTRA_PLAYER_NAME, playerName);
+        startActivityForResult(intent, REQUEST_CODE_EXAMPLE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (REQUEST_CODE_EXAMPLE == requestCode) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                if (data.getBooleanExtra(MainActivity.EXTRA_RESULT, false)) {
+                    tvTitle.setText("Bạn thắng");
+                } else {
+                    tvTitle.setText("Bạn thua");
+                }
+            }
+        }
+    }
+
+}
